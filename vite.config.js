ffb5c601a28d1df625114d8968d2680bbc1e8c74
ArknightsCalendar <backuppramadito@gmail.com>
2024-09-04 15:11:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 
 export default defineConfig({
-	base: "/arknights-calendar/",
-	build: {
-		outDir: "dist/arknights-calendar/",
-		sourcemap: true
-	},
+	// base: "/arknights-calendar/",
+	// build: {
+	// 	outDir: "dist/arknights-calendar/",
+	// 	sourcemap: true
+	// },
 	plugins: [svelte()]
 })
