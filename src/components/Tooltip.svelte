7c66a1ae31f2018d437643904c537d7712ba518f
Nep <38392948+Neppu-Nep@users.noreply.github.com>
2025-02-15 13:04:33 -0500

    Update images

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -6,7 +6,7 @@
 <div class={`${event}-tooltip ${event} tooltip`}>
     {#each Object.entries(items) as [stage, item]}
         <div class="tooltip-item">
-            <img class="tooltip-item-icon" src={`images/${item}.png`} alt={item} />
+            <img class="tooltip-item-icon" src={`materials/${item}.png`} alt={item} />
             <span class="tooltip-item-stage">{stage}</span>
         </div>
     {/each}
